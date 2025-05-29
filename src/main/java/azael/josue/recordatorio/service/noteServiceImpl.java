2434adc0f22f87b2060f3e60da9b7f33e889dc77
Josue Castro <114679940+ItsBreo@users.noreply.github.com>
2025-05-29 14:36:47 +0100

    añadido la anotacion service en noteServiceImpl

--- src/main/java/azael/josue/recordatorio/service/noteServiceImpl.java
+++ src/main/java/azael/josue/recordatorio/service/noteServiceImpl.java
@@ -1,8 +1,12 @@
 package azael.josue.recordatorio.service;
 
 import azael.josue.recordatorio.repository.noteRepository;
+
+import org.springframework.stereotype.Service;
+
 import azael.josue.recordatorio.model.note;
 
+@Service
 public class noteServiceImpl extends AbstractCrudService<note, Long> implements noteService {
 
     public noteServiceImpl(noteRepository repository) {
